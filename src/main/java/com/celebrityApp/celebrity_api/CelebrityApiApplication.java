d4c28ed72bb089b08df54ae164d8a71fccc1e333
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2024-02-11 15:43:34 +0100

    add

---
+++ src/main/java/com/celebrityApp/celebrity_api/CelebrityApiApplication.java
@@ -0,0 +1,13 @@
+package com.celebrityApp.celebrity_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebrityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebrityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celebrityApp/celebrity_api/CelebrityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celebrityApp.celebrity_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebrityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
