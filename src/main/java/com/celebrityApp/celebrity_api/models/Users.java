d81971e40f116a69e167b29a26cc94cf4f6436a5
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2024-03-10 23:09:02 +0100

    newW

--- src/main/java/com/celebrityApp/celebrity_api/models/Users.java
+++ src/main/java/com/celebrityApp/celebrity_api/models/Users.java
@@ -1,4 +1,12 @@
 package com.celebrityApp.celebrity_api.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class Users {
+
+    @Id
+    private String id;
+
 }
